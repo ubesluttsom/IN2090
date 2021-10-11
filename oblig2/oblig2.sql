c03115db0350bec2a6b17be4f84054494ddf3c90
Martin Mihle Nygaard <martin@mihle.no>
2021-10-11 12:00:35 +0200

    Gjør oppgave 3a

--- oblig2/oblig2.sql
+++ oblig2/oblig2.sql
@@ -9,9 +9,8 @@ SELECT DISTINCT count(timelistenr) FROM timeliste;
 -- (c) Hvor mange timelister som det ikke er utbetalt penger for.
 SELECT status FROM timeliste WHERE status != 'utbetalt';
 
--- (d) Antall timelistelinjer
+-- (d) Antall timelistelinjer antall timelistelinjer med en pauseverdi
 SELECT count(*) FROM timelistelinje;
---     antall timelistelinjer med en pauseverdi
 SELECT count(pause) FROM timelistelinje WHERE pause IS NOT NULL;
 
 -- (e) Alle timelistelinjer som ikke har pauseverdier (der pause er satt til
@@ -19,3 +18,10 @@ SELECT count(pause) FROM timelistelinje WHERE pause IS NOT NULL;
 SELECT count(pause) FROM timelistelinje WHERE pause IS NULL;
 
 -- OPPGAVE 3
+
+-- (a) Antall timer som det ikke er utbetalt penger for. Her kan det også være
+--     lurt å ta i bruk viewet varighet, men merk at varigheten her er i
+--     minutter, ikke timer.
+SELECT sum(varighet)/60
+  FROM timeliste NATURAL JOIN varighet
+ WHERE status != 'utbetalt';
